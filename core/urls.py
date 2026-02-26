4e3ff8fa39ef667013a7bcc2e2b1d117af5236c1
Kevin <kevin@gmail.com>
2026-02-26 03:53:00 -0500

    media

--- core/urls.py
+++ core/urls.py
@@ -23,5 +23,5 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('api/', include('posapp.urls')),
 ]
-if settings.DEBUG and settings.MEDIA_ROOT:
-    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+
+urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
